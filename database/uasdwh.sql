2b1359de69b856f2d8c11b8e36b88fe5184ecd2c
Risma4 <rismanianku4@gmail.com>
2022-01-09 06:24:14 +0700

    first commit

--- database/uasdwh.sql
+++ database/uasdwh.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2018 at 11:32 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- Generation Time: Jan 08, 2022 at 07:16 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -37,10 +38,10 @@ CREATE TABLE `alternatif` (
 --
 
 INSERT INTO `alternatif` (`id_alternatif`, `nama_alternatif`, `hasil_alternatif`) VALUES
-(1, 'Bonbin', 69.545),
-(2, 'LawangSewu', 52.665),
-(3, 'Puri Maerokoco', 80.335),
-(6, 'Taman Lele', 66.335);
+(1, 'San Francisco', 10.0005),
+(2, 'Tokyo', 61),
+(3, 'Boston', 97),
+(6, 'NYC', 83);
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,7 @@ CREATE TABLE `kriteria` (
 --
 
 INSERT INTO `kriteria` (`id_kriteria`, `nama_kriteria`, `tipe_kriteria`, `bobot_kriteria`) VALUES
-(16, 'Biaya', 'cost', 15),
+(16, 'Keindahan', 'cost', 15),
 (17, 'Jarak', 'cost', 35),
 (21, 'Fasilitas', 'benefit', 50);
 
@@ -81,18 +82,18 @@ CREATE TABLE `nilai_alternatif` (
 --
 
 INSERT INTO `nilai_alternatif` (`id_nilai_alternatif`, `ket_nilai_alternatif`, `jum_nilai_alternatif`) VALUES
-(14, 'BiayaBonbin', 60000),
-(15, 'JarakBonbin', 11),
-(16, 'FasilitasBonbin', 3),
-(18, 'BiayaLawangSewu', 40000),
-(19, 'JarakLawangSewu', 5),
-(20, 'FasilitasLawangSewu', 1),
-(21, 'BiayaPuriMaerokoco', 50000),
-(22, 'JarakPuriMaerokoco', 3),
-(23, 'FasilitasPuriMaerokoco', 2),
-(24, 'BiayaTamanLele', 50000),
-(25, 'JarakTamanLele', 5),
-(26, 'FasilitasTamanLele', 2);
+(14, 'Keindahan San Francisco', 60000),
+(15, 'JarakSan Francisco', 11),
+(16, 'FasilitasSan Francisco', 3),
+(18, 'KeindahanTokyo', 40000),
+(19, 'JarakTokyo', 5),
+(20, 'FasilitasTokyo', 1),
+(21, 'KeindahanBoston', 50000),
+(22, 'JarakBoston', 3),
+(23, 'FasilitasBoston', 2),
+(24, 'KeindahanNYC', 50000),
+(25, 'JarakNYC', 5),
+(26, 'FasilitasNYC', 2);
 
 -- --------------------------------------------------------
 
@@ -111,7 +112,7 @@ CREATE TABLE `nilai_kriteria` (
 --
 
 INSERT INTO `nilai_kriteria` (`id_nilai_kriteria`, `ket_nilai_kriteria`, `jum_nilai_kriteria`) VALUES
-(6, 'Biaya', 15),
+(6, 'Keindahan', 15),
 (7, 'Jarak', 35),
 (10, 'Fasilitas', 50);
 
@@ -135,17 +136,15 @@ CREATE TABLE `rangking` (
 
 INSERT INTO `rangking` (`id_alternatif`, `id_kriteria`, `nilai_rangking`, `nilai_normalisasi`, `bobot_normalisasi`) VALUES
 (1, 16, 60000, 0.6667, 10.0005),
-(1, 17, 11, 0.2727, 9.5445),
-(1, 21, 3, 1, 50),
 (2, 16, 40000, 1, 15),
 (2, 17, 5, 0.6, 21),
-(2, 21, 1, 0.3333, 16.665),
+(2, 21, 1, 0.5, 25),
 (3, 16, 50000, 0.8, 12),
 (3, 17, 3, 1, 35),
-(3, 21, 2, 0.6667, 33.335),
+(3, 21, 2, 1, 50),
 (6, 16, 50000, 0.8, 12),
 (6, 17, 5, 0.6, 21),
-(6, 21, 2, 0.6667, 33.335);
+(6, 21, 2, 1, 50);
 
 --
 -- Indexes for dumped tables
@@ -191,21 +190,25 @@ ALTER TABLE `rangking`
 --
 ALTER TABLE `alternatif`
   MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
   MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
 --
 -- AUTO_INCREMENT for table `nilai_alternatif`
 --
 ALTER TABLE `nilai_alternatif`
   MODIFY `id_nilai_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
 --
 -- AUTO_INCREMENT for table `nilai_kriteria`
 --
 ALTER TABLE `nilai_kriteria`
   MODIFY `id_nilai_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- Constraints for dumped tables
 --
@@ -216,6 +219,7 @@ ALTER TABLE `nilai_kriteria`
 ALTER TABLE `rangking`
   ADD CONSTRAINT `foreign1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`),
   ADD CONSTRAINT `foreign2` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
